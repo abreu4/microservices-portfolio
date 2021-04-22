1e2843b57ba03959d1609ffabdbe4a5bef00eace
Tiago Abreu <tiagoabreu7197@gmail.com>
2021-04-22 18:19:59 +0200

    First commit

---
+++ admin/consumer.py
@@ -0,0 +1,19 @@
+import pika
+from credentials import AMQPS_KEY
+
+params = pika.URLParameters(AMQPS_KEY)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+def callback(ch, method, properties, body):
+	print("Received in admin")
+	print(body)
+
+channel.basic_consume(queue='admin', on_message_callback=callback)
+
+print("Started consuming")
+
+channel.start_consuming()
+channel.close()
